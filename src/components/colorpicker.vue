d4a9c055e64d2facc4896359bfbd6a7e3afa4065
ilyeongjeong <developjiy@gmail.com>
2021-02-05 15:48:08 +0900

    ref #2 colorpicker 컴포넌트 추가

---
+++ src/components/colorpicker.vue
@@ -0,0 +1,31 @@
+<template>  
+  <colorpicker-sketch 
+    class="component-colorpicker" 
+    :value="color" 
+    @input="updateColor"
+  ></colorpicker-sketch>
+</template>
+
+<script>
+
+  import VueColor from "vue-color";
+
+  export default {
+
+    data () {
+      return {
+        color: {}
+      }
+    },
+
+    methods: {
+      updateColor (val) {
+        this.$emit("change", val.hex);
+      }
+    },
+
+    components: {
+      "colorpicker-sketch": VueColor.Sketch
+    }
+  }
+</script>
