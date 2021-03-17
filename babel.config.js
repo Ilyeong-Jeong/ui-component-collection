208ad50c5752bfbba44611683a071ed6b950ecf0
ilyeongJeong <developjiy@gmail.com>
2021-03-17 14:33:01 +0900

    ref #1 중복되는 바벨 설정 제거

--- babel.config.js
+++ babel.config.js
@@ -6,8 +6,6 @@ module.exports = function (api) {
   ];
 
   const plugins = [
-    "@babel/plugin-syntax-dynamic-import",
-    "@babel/plugin-proposal-class-properties",
     "@babel/plugin-transform-runtime"
   ];
 
